469e908ddc98afc2a58346693bc8f3e41247ea63
Jan Hornych <jan.hornych@tesena.com>
2017-03-21 09:13:58 +0100

    update

--- src/test/java/example_6/RemoteDriver.java
+++ src/test/java/example_6/RemoteDriver.java
@@ -7,11 +7,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.io.IOException;
 
@@ -19,32 +18,85 @@ public class RemoteDriver {
 
     private RemoteWebDriver driver;
 
-    @BeforeClass
+    @BeforeSuite
     public void initChrome(){
         System.setProperty("webdriver.chrome.driver", "C:\\tools\\chromedriver.exe");
     }
 
-    @Test
+    @DataProvider
+    public Object [][] dataProvider(){
+        return new Object[][]{
+                {"Issue 1", "Description"},
+                {"Issue 2", "Description 2"}};
+    }
+
+    @BeforeClass()
     public void remoteDriver() throws IOException, InterruptedException {
 
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("http://redmine.org");
+        driver.get("http://demo.redmine.org");
 
         waitForElement(By.xpath("//a[@href='/login']"), 10);
 
         WebElement login = driver.findElement(By.xpath("//a[@href='/login']"));
         login.click();
 
+        waitForElement(By.id("username"), 10);
+
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("hornyja4");
+
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("123456");
+
+        WebElement loginButton = driver.findElement(By.xpath("//input[@name='login']"));
+        loginButton.click();
+
+        waitForElement(By.id("loggedas"), 10);
+
+        WebElement projects = driver.findElement(By.xpath("//a[@href='/projects']"));
+        projects.click();
+
+        waitForElement(By.linkText("Tesena"), 10);
+
+        WebElement project = driver.findElement(By.linkText("Tesena"));
+        project.click();
+
+        waitForElement(By.xpath("//a[contains(@href, 'issues/new')]"), 10);
+
+        WebElement newIssue = driver.findElement(By.xpath("//a[contains(@href, 'issues/new')]"));
+        newIssue.click();
+
         synchronized (driver){
             driver.wait(5000);
         }
+    }
 
+    @Test(dataProvider = "dataProvider")
+    public void createIssueTest(String name, String desc){
 
+        waitForElement(By.id("issue-form"), 10);
+
+        WebElement issueSubject = driver.findElement(By.id("issue_subject"));
+        issueSubject.sendKeys(name);
+
+        WebElement issueDesc = driver.findElement(By.id("issue_description"));
+        issueDesc.sendKeys(desc);
+
+        WebElement priority = driver.findElement(By.id("issue_priority_id"));
+        Select prioritySelect = new Select(priority);
+
+        prioritySelect.selectByVisibleText("High");
+
+        WebElement submitAndContinue = driver.findElement(By.xpath("//input[@name='continue']"));
+        submitAndContinue.click();
+
+        waitForElement(By.id("flash_notice"), 10);
 
     }
 
-    @AfterClass
+    @AfterTest
     public void reset(){
         driver.quit();
     }
---
+++ src/test/java/pageObjects/LoginPage.java
@@ -0,0 +1,7 @@
+package pageObjects;
+
+/**
+ * Created by JHornych on 20.03.2017.
+ */
+public class LoginPage {
+}
